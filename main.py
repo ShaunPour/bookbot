a8d8bd484aa8e97e420593b3e10fcb0df4df38ee
shaunpour <scpour@gmail.com>
2025-02-24 10:17:45 -0500

    - Altered the existing functions to accomodate the new word_count function.
    - Now, book_text returns the contents of the file.
    
    - Following that, those are fed into the book_text variable in word_count
    which is then split into a list of words.
    
    - Finally, the length of that list is counted and the number of
    words in the text file is output to the console.

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ def get_book_text(filepath):
     try:
         with open(filepath, 'r') as file:
             content = file.read()
-            print(f"File Contents:\n {content}")
     except:
         print("Error. Filepath not found.")
+    return content
+
+def word_count():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_list = book_text.split()
+    txt_count = len(word_list)
+    print(f"{txt_count} words found in the document")
 
 def main():
-    get_book_text("books/frankenstein.txt")
-
+    word_count()
 main()
