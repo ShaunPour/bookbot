70298104ee21e260d7b31bff1c19c2007fe0161e
shaunpour <scpour@gmail.com>
2025-02-25 12:46:53 -0500

    added sorting to ensure the dictionary outputs from most characters to least

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import word_count, char_count
 
 def main():
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
     word_count()
     char_counts = char_count()
+    print("--------- Character Count -------")
     for char, count in char_counts.items():
         char_counts = char_count()
     for char, count in char_counts.items():
-        print(f"'{char}': {count}")
+        print(f"{char}: {count}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -10,15 +10,17 @@ def word_count():
     content = get_book_content()
     word_list = content.split()
     txt_count = len(word_list)
-    print(f"{txt_count} words found in the document")
+    print(f"----------- Word Count ----------\nFound {txt_count} total words")
 
 
 def char_count():
-    char_list = {}
+    char_list: dict[str, int] = {}
     words = list(get_book_content().lower())
     for char in words:
         if char in char_list:
             char_list[char] += 1
         else:
             char_list[char] = 1
-    return char_list
+    char_list_sorted = dict(sorted(char_list.items(), key=lambda x: x[1], reverse=True))
+
+    return char_list_sorted
